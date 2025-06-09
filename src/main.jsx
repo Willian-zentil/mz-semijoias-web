fdbc15786b95d95d3680b3f2c2ac25bc5d32cc0c
Willian-zentil <wzentil@gmail.com>
2025-06-09 09:37:04 -0300

    fix#extensões

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { AuthProvider } from './context/AuthContext'; // Crie este arquivo conforme abaixo
+import { AuthProvider } from './context/AuthContext.jsx'; // Crie este arquivo conforme abaixo
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
